919027015e5643ccc728479d5faffbb09725b41d
dongil033 <dongil033@gmail.com>
2015-11-08 20:35:39 +0900

    dongil

--- pupu.py
+++ pupu.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 def draw_pupu():
  import zlib,base64
- print(zlib.decompress(base64.b64decode(b'eJzdlDsWgCAMBHtOQZ37H9BCnuazJiShkgrXzDyCyJj5QfcYZbIEUwMmOkavoCU510urr5aK70fPJffWs/kqURp1WVJlRF/bwd5F/8OXIljAHmbAXcyiGVKxOVTCWZbDabZLM1zE1ob9CF8PLDKBi6vJ1AHCQc00geZdfE3Rwg7jsCTEXwGhItM9EDwKkZlm4MWgHTq1S/PO6Kge3z87ehL0aUuGhgIc8aKgZjh2p2cE4wLVQ5nK')).decode())
 if __name__ == '__main__':
  draw_pupu()
+ print "dongil"
